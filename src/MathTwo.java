43e3085c76417a10093fd9ea989589532ed5e4dd
glemma32 <genet.woldeyes@gmail.com>
2017-05-26 15:49:50 -0400

    First commit6

---
+++ src/MathTwo.java
@@ -0,0 +1,15 @@
+
+public class MathTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int myNumber=2;
+		double MyOtherNumber=1.7938;
+		
+		System.out.println(myNumber);
+		System.out.println(MyOtherNumber);
+
+	}
+
+}
